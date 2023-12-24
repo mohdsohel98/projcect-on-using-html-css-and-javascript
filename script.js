16519639b5c3998ea518b8572c850971d001c277
mohdsohel98 <khansohel8960@gmail.com>
2023-12-24 14:33:56 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+Shery.mouseFollower();
+
+Shery.makeMagnet(".magnet");
+Shery.hoverWithMediaCircle(".hvr",{videos:["0.mp4", "2.mp4","3.mp4"]});
+
+gsap.to(".fleftelem",{
+
+    scrollTrigger:{
+    trigger: "#fimages",
+     pin: true,
+     start: "top top",
+     end: "bottom bottom",
+     endTrigger: ".last",
+      scrub:1
+    },
+     y: "-300%",
+      ease: Power1
+
+});
+
+
+
+
+
+Shery.makeMagnet(".magnet-1");
+Shery.hoverWithMediaCircle(".hvr",{videos:["0.mp4", "2.mp4","3.mp4"]});
+
+
+
+
+//  let sections = document.querySelectorAll(".fleftelem");
+
+// Shery.imageEffect(".images", {
+//     style: 1,
+//     debug:true,
+//     config:{onMouse:{value: 1}},
+//     slideStyle: (setScroll) => {
+//         sections.forEach(function(section,index){
+//           ScrollTrigger.create({
+//             trigger: section,
+//             start:"top top",
+//             //  scrub:1,
+//               onUpdate:function(prog){
+//                 setScroll(prog.progress+index)
+//               },
+
+//           });
+//         });
+      
+//     },
+//   });
+
+
